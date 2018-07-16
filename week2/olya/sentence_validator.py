1ebc20c540473f4b5cd3f7ff3ab5e46628c9ad3b
Olya <o.vardarova@gmail.com>
2018-07-16 00:38:47 -0700

    Add sentence validator
    Signed-off-by: Olya <o.vardarova@gmail.com>

---
+++ week2/olya/sentence_validator.py
@@ -0,0 +1,27 @@
+import re
+
+def sentence_validator(text):
+    """
+    Check if all sentences in the text are valid
+        Args: string
+        Returns: true or false
+    """
+
+    pattern = "[.?!]"
+    sentences = re.split(pattern, text)
+
+    for sentence in sentences[:-1]:
+        word = sentence.strip()
+        if word[0].islower():
+            return False
+    for sentence in sentences[:-1]:
+        comma_pattern = ",(?=\S)|:"
+        commas = re.split(comma_pattern, sentence)
+        if commas.__len__() != 1:
+            return False
+    return True
+
+
+test_text = "Sed placerat ligula libero! Donec eu blandit mi. Sed sed nisi ipsum. Donec facilisis sagittis enim, a dignissim augue gravida at. Nullam quis diam scelerisque, sodales ante ac, condimentum nibh? "
+a = sentence_validator(test_text)
+print(a)
