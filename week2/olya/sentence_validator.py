9e69a7e2eb137293c60981dcb809818acfdfec63
Olya <o.vardarova@gmail.com>
2018-07-23 00:07:10 -0700

    Add update on sentence validator
    Signed-off-by: Olya <o.vardarova@gmail.com>

--- week2/olya/sentence_validator.py
+++ week2/olya/sentence_validator.py
@@ -1,29 +1,25 @@
 import re
 
+
 def sentence_validator(text):
     """
-    Check  if all sentences in the text are valid
+    Checks if all sentences in the text are valid
         Args: string
         Returns: true or false
     """
+    sentence_pattern = "[.?!]"
+    comma_pattern = ",(?=\S)"
+    sentences = re.split(sentence_pattern, text)
 
-    pattern = "[.?!]"
-    sentences = re.split(pattern, text)
-
-    for sentence in sentences[:-1]:
+    for sentence in sentences:
         word = sentence.strip()
-        if word[0].islower():
-            return False
-    for sentence in sentences[:-1]:
-        comma_pattern = ",(?=\S)|:"
-        commas = re.split(comma_pattern, sentence)
-        if commas.__len__() != 1:
-            return False
+        if word != "":
+            if word[0].islower():
+                return False
+            commas = re.split(comma_pattern, sentence)
+            if len(commas) != 1:
+                return False
+        else:
+            return True
+
     return True
-
-
-test_text = "Sed placerat ligula libero! Donec eu blandit mi. Sed sed nisi ipsum. Donec facilisis sagittis enim, a dignissim augue gravida at. Nullam quis diam scelerisque, sodales ante ac, condimentum nibh? "
-a = sentence_validator(test_text)
-print(a)
-
-
